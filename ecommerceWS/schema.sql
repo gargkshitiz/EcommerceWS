9f950bed15308be0fda77df90de8ccadedce0496
Kshitiz Garg <gargkshitiz@gmail.com>
2017-01-24 22:01:22 +0530

    Updated documentation and schema

--- ecommerceWS/schema.sql
+++ ecommerceWS/schema.sql
@@ -9,14 +9,14 @@ GO
 
 --DROP OLD TABLES
 -----------------------
-IF OBJECT_ID('dbo.Product', 'U') IS NOT NULL  DROP TABLE dbo.Product; 
-IF OBJECT_ID('dbo.Category', 'U') IS NOT NULL  DROP TABLE dbo.Category; 
-IF OBJECT_ID('dbo.ProductCategory', 'U') IS NOT NULL  DROP TABLE dbo.ProductCategory; 
+IF OBJECT_ID('dbo.Product', 'U') IS NOT NULL DROP TABLE dbo.Product; 
+IF OBJECT_ID('dbo.Category', 'U') IS NOT NULL DROP TABLE dbo.Category; 
+IF OBJECT_ID('dbo.ProductCategory', 'U') IS NOT NULL DROP TABLE dbo.ProductCategory; 
 
 CREATE TABLE [dbo].[Product](
 	[ProductId] BIGINT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[ProductDesc] VARCHAR(450) NULL,
-	[ProductCurrency] VARCHAR(450) NULL,
+	[ProductDesc] VARCHAR(450) NOT NULL,
+	[ProductCurrency] VARCHAR(450) NOT NULL,
 	[Price] VARCHAR(450) NOT NULL,
 	[ProductCode] VARCHAR(450) NOT NULL,
 	[ProductType] VARCHAR(450) NOT NULL,
@@ -28,8 +28,8 @@ GO
 
 CREATE TABLE [dbo].[Category](
 	[CategoryId] BIGINT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[CategoryDesc] VARCHAR(450) NULL,
-	[CategoryName] VARCHAR(450) NULL,
+	[CategoryDesc] VARCHAR(450) NOT NULL,
+	[CategoryName] VARCHAR(450) NOT NULL,
 	[ParentCategoryId] [bigint] NOT NULL,
 	[CreatedAt] [datetime] NOT NULL,
 	[LastModifiedAt] [datetime] NOT NULL)
