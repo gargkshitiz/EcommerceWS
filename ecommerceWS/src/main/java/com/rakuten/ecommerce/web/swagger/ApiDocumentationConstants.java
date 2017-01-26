5e780a212a702954f91ad52eae116621a9a3b66a
Kshitiz Garg <gargkshitiz@gmail.com>
2017-01-26 14:07:50 +0530

    Updated documentation

--- ecommerceWS/src/main/java/com/rakuten/ecommerce/web/swagger/ApiDocumentationConstants.java
+++ ecommerceWS/src/main/java/com/rakuten/ecommerce/web/swagger/ApiDocumentationConstants.java
@@ -27,7 +27,7 @@ public interface ApiDocumentationConstants {
 	String PRODUCT_GET = "Fetches an existing product from ecommerceWS";
 	String PRODUCT_GET_NOTES = "Here client could also request to see the price of the product in a desiredPriceCurrency (alongwith prices in EURO, which comes back by default) using a query parameter. Here associated categories are being returned as well (which are lazily loaded in the backend)";
 	String PRODUCT_PUT = "Updates a product's details in ecommerceWS";
-	String PRODUCT_PUT_NOTES = "We could change productDesc, productCurrency, price, catgeoryIds, unitsInStock, productCode, productType etc. New price in a new currency can also be given here";
+	String PRODUCT_PUT_NOTES = "We could change productDesc, productCurrency, price, catgeoryIds, unitsInStock, productCode, productType etc. Previously associated categoryIds would be simply replaced by the one provided in the request. New price in a new currency can also be given here";
 	String PRODUCT_DELETE = "Deletes a product and all of its category mappings from ecommerceWS";
 	String PRODUCT_DELETE_NOTES = "Categories would not be impacted, only the relavant productCategory mappings would be wiped off";
 	
@@ -37,7 +37,7 @@ public interface ApiDocumentationConstants {
 	String PRODUCTS_POST = "Creates a new product in  ecommerceWS";
 	String PRODUCTS_POST_NOTES = "Input catgeoryIds could be valid category id(s) OR absent (no category for product at the moment). Check 'location' header for the create productId";
 	
-	String PRODUCTS_GET = "Fetches products from ecommerceWS";
+	String PRODUCTS_GET = "Bulk-Fetch, Fetches products from ecommerceWS";
 	String PRODUCTS_GET_NOTES = "Results are fetched up to a pre-defined limit starting from startingProductId (inclusive). In the response, if hasMore is false and nextProductId is -1, that means no further data is available. If hasMore is true, then there would be as many items as the count attribute. Client could then send nextProductId (as received in the previous response) as the startingProductId as a query parameter to fetch further results";
 	
 	String CATEGORY = "category";
@@ -56,7 +56,7 @@ public interface ApiDocumentationConstants {
 	String CATEGORIES_POST = "Creates a new category in  ecommerceWS";
 	String CATEGORIES_POST_NOTES = "From the request, parent category Id could be absent (no parent) OR -1 (no parent) or a valid category id (valid parent)";
 
-	String CATEGORIES_GET = "Fetches categories from ecommerceWS";
+	String CATEGORIES_GET = "Bulk-Fetch, fetches categories from ecommerceWS";
 	String CATEGORIES_GET_NOTES = "Results are fetched up to a pre-defined limit starting from startingCategoryId (inclusive). In the response, if hasMore is false and nextCategoryId is -1, that means no further data is available. If hasMore is true, then there would be as many items as the count attribute. Client could then send nextCategoryId (as received in the previous response) as the startingCategoryId as a query parameter to fetch further results";
 
 }
